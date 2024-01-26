b18bfaaa4204ce0e267217510e790677f665c1a9
iTSiolinux <easy07770@gmail.com>
2024-01-26 12:54:51 +0200

    [X] (1) summon Player entity and manage to move it around [X] (2) create new clean Block class

--- client/backend/JS/Game.js
+++ client/backend/JS/Game.js
@@ -7,11 +7,12 @@ const Game = {
     awake: () => {
         console.log("Game is awaked!")
         // registering some events listeners before awaking the game
-        window.addEventListener('resize', Game.events.onResize)
+        document.addEventListener('resize', Game.events.onResize)
 
         // keyboard event
-        window.addEventListener('keydown', Game.events.onKeyDown)
-        window.addEventListener('keyup', Game.events.onKeyUp)
+        document.addEventListener('keydown', Game.events.onKeyDown)
+        document.addEventListener('keypress', Game.events.onKeyPress)
+        document.addEventListener('keyup', Game.events.onKeyUp)
 
         // mouse event
         Game.canvas.addEventListener('mousemove', Game.events.onMouseMove)
@@ -27,6 +28,9 @@ const Game = {
         const p = new Player({POS: {x: 64, y: 64}, texture: Texture.getImage("player")})
         Game.arrays.entitys.push(p)
         Game.player = p;
+
+        // Attaching player to the camera as target
+        Game.Camera.target = p; 
 
         // Test block
         const b = new Block({texture: Texture.getImage("oak"), w: 2, h: 2})
@@ -114,6 +118,11 @@ Game.events = {
         Game.keyboard.last.event = event;
         Game.keyboard.last.onKeyDown = event;
     },
+    onKeyPress: (event) => {
+        Game.events.handler(event);
+        Game.keyboard.last.event = event;
+        Game.keyboard.last.onKeyPress = event;
+    },
     onKeyUp: (event) => {
         Game.events.handler(event);
         Game.keyboard.last.event = event;
@@ -142,8 +151,8 @@ Game.events = {
 
         } else if (event instanceof MouseEvent) {
             // setting the mouse position by the client pos
-            Game.mouse.data.position.window.x = event.clientX - Game.canvas.offsetLeft - Game.canvas.width / 2;
-            Game.mouse.data.position.window.y = event.clientY - Game.canvas.offsetTop - Game.canvas.height / 2;
+            Game.mouse.data.position.window.x = event.clientX + Game.canvas.offsetLeft - Game.canvas.width / 2;
+            Game.mouse.data.position.window.y = event.clientY + Game.canvas.offsetTop - Game.canvas.height / 2;
 
             // calculating realative to the canvas on Game.Camera.follow
         } else {
@@ -157,6 +166,7 @@ Game.keyboard = {
     last: {
         event: null,
         onKeyDown: null,
+        onKeyPress: null,
         onKeyUp: null
     },
     array: [],
@@ -165,28 +175,28 @@ Game.keyboard = {
         const POS = {x: 0, y: 0}
 
         // up key
-        if (keys[87] == "keydown"){
+        if (keys[87] == "keypress"){
             POS.y--;
         } else if (keys[87] == "keyup"){
             POS.y++;
         }
 
         // down key
-        if (keys[83] == "keydown"){
+        if (keys[83] == "keypress"){
             POS.y++;
         } else if (keys[83] == "keyup"){
             POS.y--;
         }
 
         // left key
-        if (keys[65] == "keydown"){
+        if (keys[65] == "keypress"){
             POS.x--;
         } else if (keys[65] == "keyup"){
             POS.x++;
         }
 
         // right key
-        if (keys[68] == "keydown"){
+        if (keys[68] == "keypress"){
             POS.x++;
         } else if (keys[68] == "keyup"){
             POS.x--;
